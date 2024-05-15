23559cfc59b591e22ada8b2818223603dfc45a99
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-05-15 10:29:48 +0700

    ....

--- huynhtinthestore/src/components/Navbar.jsx
+++ huynhtinthestore/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-      
+    <div className='text-3xl'>
+      <p className='text-red-500'>Navbar</p>
     </div>
   )
 }
---
+++ huynhtinthestore/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function Home (){
+    return (
+        <div>
+            <p className='text-red-500'>Home</p>
+        </div>
+    );
+}
+
+export default Home;
+
