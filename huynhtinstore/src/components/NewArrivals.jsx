a633838035f728e64dbac0994de309f0992d7b59
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-05-18 22:24:44 +0700

    ...

--- huynhtinstore/src/components/NewArrivals.jsx
+++ huynhtinstore/src/components/NewArrivals.jsx
@@ -8,7 +8,7 @@ const NewArrivals = () => {
             <p className='text-center text-[16px] font-[Poppins] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices sollicitudin  </p>
             <ButtonGroud />
             <div className='grid grid-cols-4  items-center justify-center'>
-                <div className=' shadow rounded-lg m-2 flex items-center justify-center'>
+                <div className=' shadow cursor-pointer hover:translate-y-5 rounded-lg m-4 flex items-center justify-center'>
                     <div className='w-[90%]'>
                         <img className=' h-[300px] w-full mt-4 rounded-xl' src="https://i.pinimg.com/564x/fc/13/49/fc1349ef42419e0d3dd33c717bfae33a.jpg" alt="" />
                         <div className='flex mt-4'>
@@ -32,7 +32,7 @@ const NewArrivals = () => {
                     </div>
                 </div>
 
-                <div className=' shadow rounded-lg m-2 flex items-center justify-center'>
+                <div className=' shadow cursor-pointer hover:translate-y-5 rounded-lg m-4 flex items-center justify-center'>
                     <div className='w-[90%]'>
                         <img className=' h-[300px] w-full mt-4 rounded-xl' src="https://i.pinimg.com/564x/c3/5c/9a/c35c9ae55d95f354cc9312ce98cbd413.jpg" alt="" />
                         <div className='flex mt-4'>
@@ -56,7 +56,7 @@ const NewArrivals = () => {
                     </div>
                 </div>
 
-                <div className=' shadow rounded-lg m-2 flex items-center justify-center'>
+                <div className=' shadow cursor-pointer hover:translate-y-5 rounded-lg m-4 flex items-center justify-center'>
                     <div className='w-[90%]'>
                         <img className=' h-[300px] w-full mt-4 rounded-xl' src="https://i.pinimg.com/564x/5b/80/87/5b8087028b5e91175ad3a203f305c945.jpg" alt="" />
                         <div className='flex mt-4'>
@@ -80,7 +80,31 @@ const NewArrivals = () => {
                     </div>
                 </div>
 
-                <div className=' shadow rounded-lg m-2 flex items-center justify-center'>
+                <div className=' shadow cursor-pointer hover:translate-y-5 rounded-lg m-4 flex items-center justify-center'>
+                    <div className='w-[90%]'>
+                        <img className=' h-[300px] w-full mt-4 rounded-xl' src="https://i.pinimg.com/564x/2a/71/ce/2a71ce1af6473f25a7a48fd7b766c939.jpg" alt="" />
+                        <div className='flex mt-4'>
+                            <div className='w-[60%]'>
+                                <p className='text-[20px] text-[#484848]' >Shiny Dress</p>
+                            </div>
+                            <div className='w-[40%] flex items-center justify-end'>
+                                <StarIcon className='text-[#F2994A]' />
+                                <StarIcon className='text-[#F2994A]' />
+                                <StarIcon className='text-[#F2994A]' />
+                                <StarIcon className='text-[#F2994A]' />
+                                <StarIcon className='text-[#F2994A]' />
+                            </div>
+                        </div>
+                        <p className='text-[#8A8A8A] text-[12px]'>Al Karam</p>
+                        <p className='text-[#484848] text-[12px] mt-4 mb-4'><span>(4.1k)</span> Customer Reviews</p>
+                        <div className='flex  '>
+                            <p className='text-[#484848] text-[24px] w-[70%]'>$95.50</p>
+                            <p className='text-[#FF4646] text-[12px] w-[30%] mt-2'>Almost Sold Out</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className=' shadow cursor-pointer hover:translate-y-5 rounded-lg m-4 flex items-center justify-center'>
                     <div className='w-[90%]'>
                         <img className=' h-[300px] w-full mt-4 rounded-xl' src="https://i.pinimg.com/564x/2a/71/ce/2a71ce1af6473f25a7a48fd7b766c939.jpg" alt="" />
                         <div className='flex mt-4'>
