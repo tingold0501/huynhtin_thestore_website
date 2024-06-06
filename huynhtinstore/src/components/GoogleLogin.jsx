bc3c90dab4471754d33881fcf228bdb205ccd3c4
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-06-06 13:29:36 +0700

    .....

--- huynhtinstore/src/components/GoogleLogin.jsx
+++ huynhtinstore/src/components/GoogleLogin.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
 const Googlelogin = () => {
-
+    const handleError = (response) => {
+        console.log(response);
+    }
+    const handleSuccess = (response) => {
+        const googleUser = jwtDecode(response.credential);
+        const newData = {
+            id : googleUser.iat,
+            avatar : googleUser.picture,
+            firstname : googleUser.given_name,
+            lastName : googleUser.family_name,
+            email : googleUser.email,
+            password : googleUser.jti,
+            isAdmin : false
+          }
+        localStorage.setItem('token', response.credential);
+        localStorage.setItem('user', JSON.stringify(newData));
+        window.location.replace('/');
+    }
     return (
         <div>
             <GoogleLogin
-                onSuccess={credentialResponse => {
-                    console.log(credentialResponse);
-                    localStorage.setItem('token', credentialResponse.credential);
-                }}
-                onError={() => {
-                    console.log('Login Failed');
-                }}
+                onSuccess={handleSuccess}
+                onError={handleError}
             />
         </div>
     );
--- huynhtinstore/src/components/Navbar.jsx
+++ huynhtinstore/src/components/Navbar.jsx
@@ -12,6 +12,15 @@ import NewArrivals from './NewArrivals';
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
+    const [token, setToken] = useState(localStorage.getItem('token'));
+    const userInfo = JSON.parse(localStorage.getItem('user'));
+
+
+    const handleLogut = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        setToken(null);
+    }
     return (
         <nav className="mt-12 w-[80%] ">
             <ul className='flex items-center justify-between font-[regular]'>
@@ -20,15 +29,37 @@ const Navbar = () => {
                 <li className='text-[16px] text-[#484848] hover:text-gray-500 hover:underline hover:underline-offset-8 cursor-pointer'>Deals</li>
                 <li className='text-[16px] text-[#484848] hover:text-gray-500 hover:underline hover:underline-offset-8 cursor-pointer'>New Arrivals</li>
                 <li className='text-[16px] text-[#484848] hover:text-gray-500 hover:underline hover:underline-offset-8 cursor-pointer'>Packages</li>
-                {/* {!token && (
-                     <Link to="">
-                     <li className='text-[16px] text-[#484848] hover:bg-[#000000] rounded-lg hover:text-white w-[152px] h-[56px]'><button className='w-full  h-full'>Sign Out</button></li>
-                 </Link>
-                )} */}
-                <Link to="/login">
-                    <li className='text-[16px] text-[#484848] hover:bg-[#000000] rounded-lg hover:text-white w-[152px] h-[56px]'><button className='w-full  h-full'>Sign In</button></li>
-                </Link>
-                <li className='text-[16px] text-[#484848] bg-slate-500 hover:bg-[#000000] rounded-lg hover:text-white w-[152px] h-[56px]'><button className='w-full  h-full'>Sign Up</button></li>
+
+                {token ? (
+                    <div className='flex items-center cursor-pointer '>
+                        <div onClick={handleLogut} className="center relative inline-block select-none whitespace-nowrap rounded-full bg-teal-500 px-3.5 hover:bg-slate-500 py-1.5 align-baseline font-sans text-xs font-bold uppercase leading-none text-white">
+                            <div className="absolute top-2/4 left-1 h-5 w-5 -translate-y-2/4">
+                                <img
+                                    alt="tania andrew"
+                                    src={userInfo.avatar}
+                                    className="relative inline-block h-5 w-5 translate-x-px translate-y-px rounded-full object-cover object-center"
+                                />
+                            </div>
+                            <div className="ml-4 mt-px">
+                                <p className="block font-sans text-sm font-medium capitalize leading-none text-white antialiased">
+                                    {userInfo.firstname + ' ' + userInfo.lastName}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                ) : (
+                    <div className='flex'>
+                        <Link to="/login">
+                            <li className='text-[16px] text-[#484848] hover:bg-[#000000] rounded-lg hover:text-white w-[152px] h-[56px] mr-4'><button className='w-full  h-full'>Sign In</button></li>
+                        </Link>
+                        <Link>
+                            <li className='text-[16px] text-[#484848] bg-slate-500 hover:bg-[#000000] rounded-lg hover:text-white w-[152px] h-[56px]'><button className='w-full  h-full'>Sign Up</button></li>
+
+                        </Link>
+                    </div>
+
+                )}
             </ul>
             <div className='w-full flex  mt-[10%] '>
                 <div className=' w-full '>
