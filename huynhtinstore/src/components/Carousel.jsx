95e3ba2f2204f058c779e27a1ca62821f5f271c0
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-06-21 22:07:42 +0700

    .....

--- huynhtinstore/src/components/Carousel.jsx
+++ huynhtinstore/src/components/Carousel.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
-const Carousel = () => {
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import ImageCarousel from '../assets/images/imagecarousel.png';
+import AdjustIcon from '@mui/icons-material/Adjust';
+const Carousel = ({children:slides}) => {
     return (
         <div className=' mt-32 w-full bg-[#eeeeee] h-[782px] flex items-center justify-end'>
-            <div className='w-[85%] h-[80%] '>
+            <div className='w-[85%] h-[80%] flex'>
                 <div className='w-[30%] h-full flex-col space-y-10 '>
                     <div className='mt-[10%] flex flex-col space-y-10'>
                         <p className='text-[46px] text-[#484848]'>Deals Of The Month</p>
@@ -12,22 +14,58 @@ const Carousel = () => {
                         <p className='text-[#484848] text-[28px]'>Hurry, Before It’s Too Late!</p>
                     </div>
                     <div className='flex flex-row justify-between'>
-                        <div className='w-[76px] h-[76px] bg-white shadow-xl rounded-lg'>
-
+                        <div>
+                            <div className='w-[76px] h-[76px] bg-white shadow-xl rounded-lg flex items-center justify-center'>
+                                <p className='text-[32px]'>02</p>
+                            </div>
+                            <p className=' text-center text-[24px] mt-2'>Days</p>
                         </div>
-                        <div className='w-[76px] h-[76px] bg-white shadow-xl rounded-lg'>
-
+                        <div>
+                            <div className='w-[76px] h-[76px] bg-white shadow-xl rounded-lg flex items-center justify-center'>
+                                <p className='text-[32px]'>02</p>
+                            </div>
+                            <p className=' text-center text-[24px] mt-2'>Hr</p>
                         </div>
-                        <div className='w-[76px] h-[76px] bg-white shadow-xl rounded-lg'>
-
+                        <div>
+                            <div className='w-[76px] h-[76px] bg-white shadow-xl rounded-lg flex items-center justify-center'>
+                                <p className='text-[32px]'>02</p>
+                            </div>
+                            <p className=' text-center text-[24px] mt-2'>Mins</p>
                         </div>
-                        <div className='w-[76px] h-[76px] bg-white shadow-xl rounded-lg'>
-
+                        <div>
+                            <div className='w-[76px] h-[76px] bg-white shadow-xl rounded-lg flex items-center justify-center'>
+                                <p className='text-[32px]'>02</p>
+                            </div>
+                            <p className=' text-center text-[24px] mt-2'>SEC</p>
                         </div>
                     </div>
                 </div>
-                <div className='w-[65%]'>
+                <div className='w-[75%] flex relative'>
+                    <div className='w-[15%] h-full flex flex-col justify-end ml-5'>
+                        <div>
+                            <button className='w-[48px] h-[48px] bg-white rounded-full mr-3 transform rotate-180' type="button"><NavigateNextIcon /></button>
+                            <button className='w-[48px] h-[48px] bg-white rounded-full ' type="button"><NavigateNextIcon /></button>
+                        </div>
+                    </div>
+                    <div className='w-full h-full grid grid-cols-3 gap-6'>
+                        <div>
+                            <img className='w-full h-full' src={slides} alt="" srcset="" />
+                        </div>
+                        <div>
+                            <img className='w-full h-[486px]' src={ImageCarousel} alt="" srcset="" />
+                            <div className='mt-5'>
+                                <AdjustIcon />
+                            </div>
+                        </div>
+                        <img className='w-full h-[486px]' src={ImageCarousel} alt="" srcset="" />
 
+                    </div>
+                    <div className='absolute top-[450px] right-[750px] w-[217px] h-[130px] bg-white flex flex-col justify-center'>
+                        <div className='ml-5'>
+                            <p className='text-[16px]'>01 - <span>Spring Sale</span></p>
+                            <p className='text-[28px]'>30% <span>OFF</span></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- huynhtinstore/src/components/Navbar.jsx
+++ huynhtinstore/src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import Header from './Header';
 import Banner from './Banner';
 import Brand from './Brand';
 import ButtonFixed from './ButtonFixed';
+import ImageCarousel from '../assets/images/imagecarousel.png';
 
 const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false);
@@ -13,6 +14,12 @@ const Navbar = () => {
         localStorage.removeItem("token");
         window.location.reload();
     }
+
+    const slides = [
+        ImageCarousel,
+        ImageCarousel,
+        ImageCarousel,
+    ]
     return (
         <div className='w-[70%] relative'>
             <Header/>
