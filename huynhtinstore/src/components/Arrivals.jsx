6d2df191de098fc87255a10aaf92fb9db6c4ee8c
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-06-24 18:13:25 +0700

    ......

--- huynhtinstore/src/components/Arrivals.jsx
+++ huynhtinstore/src/components/Arrivals.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ArrivalsItem from './ArrivalsItem';
 const Arrivals = () => {
     return (
-        <div className='w-[70%] flex flex-col justify-center items-center'>
+        <div className='w-[70%] flex flex-col justify-center items-center mt-20'>
             <div className='w-full flex flex-col items-center justify-center'>
-                <div className='w-[30%] text-center'>
-                    <p>New Arrivals</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices sollicitudin </p>
+                <div className='w-[40%] text-center'>
+                    <p className='text-[46px] text-[#484848]'>New Arrivals</p>
+                    <p className='text-[#8A8A8A] text-[16px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices sollicitudin </p>
                 </div>
             </div>
-            <div className='w-full grid grid-cols-3 items-center justify-between'>
+            <div className='w-full grid grid-cols-3 items-center justify-between gap-10'>
+                <ArrivalsItem />
                 <ArrivalsItem />
                 <ArrivalsItem />
                 <ArrivalsItem />
--- huynhtinstore/src/components/ArrivalsItem.jsx
+++ huynhtinstore/src/components/ArrivalsItem.jsx
@@ -3,20 +3,31 @@ import StarIcon from '@mui/icons-material/Star';
 
 const ArrivalsItem = () => {
     return (
-        <div className='w-full'>
-            <div className='w-full '>
-                <img className='h-[300px]' src="https://i.pinimg.com/564x/fe/75/d2/fe75d2125bb319a4d3d22b9d81fd308d.jpg" alt="" />
+        <div className='cursor-pointer w-full h-full shadow-2xl rounded-3xl shadow-gray-400 flex items-center justify-center'>
+            <div className='w-[90%] h-[450px] mt-6'>
+                <div className='w-full h-[250px] bg-slate-500 rounded-3xl'>
+                    <img className='h-full w-full rounded-3xl object-fill hover:object-scale-down' src="https://i.pinimg.com/564x/fe/75/d2/fe75d2125bb319a4d3d22b9d81fd308d.jpg" alt="" />
+                </div>
+                <div className='w-full flex justify-between mt-4'>
+                    <p className='text-[20px] text-[#484848]'>Shiny Dress</p>
+                    <div className='text-[#FCA120]'>
+                        <StarIcon />
+                        <StarIcon />
+                        <StarIcon /> 
+                        <StarIcon />
+                        <StarIcon />
+                        <StarIcon />
+                    </div>
+
+                </div>
+                <p className='text-[#8A8A8A] text-[12px]'>Al Karam</p>
+                <p className='text-[#8A8A8A] text-[12px] mt-6 mb-6'><span>(4.1k)</span> Customer Reviews</p>
+                <div className='flex justify-between'>
+                    <p className=' text-[24px]'>$95.50</p>
+                    <p className='text-[#FF4646] text-[12px] mt-3'>Almost Sold Out</p>
+                </div>
             </div>
-            <div className='w-full flex flex-row items-end'>
-                <p>Shiny Dress</p>
-                <StarIcon />
-            </div>
-            <p>Al Karam</p>
-            <p><span>(4.1k)</span> Customer Reviews</p>
-            <div>
-                <p>$95.50</p>
-                <p>Almost Sold Out</p>
-            </div>
+
         </div>
     );
 }
